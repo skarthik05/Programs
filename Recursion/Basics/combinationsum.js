408e9be8e548bc1e446098d328568435e2566e99
skarthik05 <karthiks0598@gmail.com>
2022-08-17 08:21:22 +0530

    Added sumcombination without allowing the duplicates

--- Recursion/Basics/combinationsum.js
+++ Recursion/Basics/combinationsum.js
@@ -1,3 +1,4 @@
+//With duplicates
 function sumCombinaton(index, target, arr, ans, ds) {
   if (index == arr.length) {
     if (target == 0) {
@@ -20,3 +21,36 @@ function callsubcomb(nums) {
 const arr = [2, 3, 6, 7];
 const result = callsubcomb(arr);
 console.log({ result });
+
+//TS : O(2^T * K) //Where k is the DS to put into another DS
+
+//Without allowing Duplicates
+
+function sumCombinationWithoutDuplicates(index, target, arr, ans, ds) {
+  //found the sum
+  if (target === 0) {
+    ans.push([...ds]);
+    return;
+  }
+  for (let i = index; i < arr.length; i++) {
+    //i > index, pick if the element is appearing for the first time
+    //arr[i] ==arr[i-1], to avoid the duplicate combination
+    if (i > index && arr[i] == arr[i - 1]) continue;
+    if (arr[i] > target) break;
+    ds.push(arr[i]);
+    sumCombinationWithoutDuplicates(i + 1, target - arr[i], arr, ans, ds);
+    ds.pop();
+  }
+}
+function withoutDuplicate(nums) {
+  const ans = [];
+  sumCombinationWithoutDuplicates(0, 4, nums, ans, []);
+  return ans;
+}
+const arr1 = [1, 1, 1, 2, 2];
+const result1 = withoutDuplicate(arr1);
+console.log({ result1 });
+
+//TS: O(2^N * K) Where '2^N' for all the unqiue combination(i.e., Number of subseq) for recursion,
+//'K' is the ds to insert into another ds(i.e., avg length of every combination) //for adding values
+//SC: O(K*X) Where 'K' is the avg length and 'X' is the combination and if i am ignoring the auxilary space of recursion
