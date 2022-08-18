ec5770aa0ac75349b615052417d6f4969a20fb6f
skarthik05 <karthiks0598@gmail.com>
2022-08-18 08:45:11 +0530

    Added subset sum variation

---
+++ Recursion/Basics/subsetsum.js
@@ -0,0 +1,39 @@
+function helpersubsetsum(index, sum, arr, ans) {
+  if (index === arr.length) {
+    console.log({ sum });
+    ans.push(sum);
+    return;
+  }
+  //To pick up the index value
+  helpersubsetsum(index + 1, sum + arr[index], arr, ans);
+  //To Skip the index value
+  helpersubsetsum(index + 1, sum, arr, ans);
+}
+function subsetsum(nums) {
+  const ans = [];
+  helpersubsetsum(0, 0, nums, ans);
+  ans.sort((a, b) => a - b);
+  return ans;
+}
+// const nums = [3, 2, 1];
+// const result = subsetsum(nums);
+// console.log({ result });
+
+function helpersubsetsum1(index, num1, ds, ans) {
+  ans.push([...ds]);
+  for (let i = index; i <= num1.length - 1; i++) {
+    if (i != index && num1[i] == num1[i - 1]) continue;
+    ds.push(num1[i]);
+    console.log({ ds });
+    helpersubsetsum1(i + 1, num1, ds, ans);
+    ds.pop();
+  }
+}
+function subsetsum1(num1) {
+  const ans = [];
+  helpersubsetsum1(0, num1, [], ans);
+  return ans;
+}
+const num1 = [1, 2, 2];
+const result1 = subsetsum1(num1);
+console.log({ result1 });
